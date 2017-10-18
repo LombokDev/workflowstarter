f3a6b623b9ec49f2c4d9d2fe44c1899080045616
Khairul Imam <ki65559@gmail.com>
2017-10-18 11:29:58 +0800

    echo with new line

--- starter.sh
+++ starter.sh
@@ -3,38 +3,38 @@ mkdir docs slides
 touch {acara,evaluasi,keuangan}.md
 touch docs/index.html slides/README.md
 acaradefaultkonten="
-## Konsep
-{Lightning Talk, Tech Talk, Instant Talk, Bootcamp, Hackathon}
+## Konsep\n
+{Lightning Talk, Tech Talk, Instant Talk, Bootcamp, Hackathon}\n
 
-## Susunan acara
-| WAKTU         | PEMBICARA                  | AGENDA                                             |
-|---------------|----------------------------|----------------------------------------------------|
-
-## Aturan
+## Susunan acara\n
+| WAKTU         | PEMBICARA                  | AGENDA                                             |\n
+|---------------|----------------------------|----------------------------------------------------|\n
+\n
+## Aturan\n
 "
 
 evaluasidefaultkonten="
-## Persentase kehadiran
-
-## Masukan dan kritik
-- point 1
-- point 2
+## Persentase kehadiran\n
+\n
+## Masukan dan kritik\n
+- point 1\n
+- point 2\n
 "
 
 keuangandefaultkonten="
-## Rincian dana masuk
-| Sumber          | Jumlah        |
-|-----------------|---------------|
-
-## Rincian Pengeluaran
-
-### Pengeluaran 1
-Detail dan bukti
-### Pengeluaran 2 dan seterusnya
+## Rincian dana masuk\n
+| Sumber          | Jumlah        |\n
+|-----------------|---------------|\n
+\n
+## Rincian Pengeluaran\n
+\n
+### Pengeluaran 1\n
+Detail dan bukti\n
+### Pengeluaran 2 dan seterusnya\n
 "
 
-echo $acaradefaultkonten > acara.md
-echo $evaluasidefaultkonten > evaluasi.md
-echo $keuangandefaultkonten > keuangan.md
+echo -e $acaradefaultkonten > acara.md
+echo -e $evaluasidefaultkonten > evaluasi.md
+echo -e $keuangandefaultkonten > keuangan.md
 echo "halaman publikasi" > docs/index.html
 echo "Semua file presentasi bisa dilihat disini" > slides/README.md
